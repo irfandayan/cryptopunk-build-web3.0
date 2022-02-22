28397fe757f53eeb3acb99bd47597e0f6333b5e7
irfan dayan <engr.irfandayan@gmail.com>
2022-02-22 17:20:26 +0500

    Complete header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,8 @@ import React from "react";
 
 import "./Header.css";
 import punkLogo from "../assets/header/cryptopunk-logo.png";
+import searchIcon from "../assets/header/search.png";
+import themeSwitchIcon from "../assets/header/theme-switch.png";
 
 const Header = () => {
   return (
@@ -9,6 +11,31 @@ const Header = () => {
       <div className="logoContainer">
         <img src={punkLogo} className="punkLogo" alt="" />
       </div>
+
+      <div className="searchBar">
+        <div className="searchIconContainer">
+          <img src={searchIcon} alt="" />
+        </div>
+
+        <input
+          className="searchInput"
+          placeholder="Collection, item or user..."
+        />
+      </div>
+
+      <div className="headerItems">
+        <p>Drops</p>
+        <p>Marketplace</p>
+        <p>Create</p>
+      </div>
+
+      <div className="headerActions">
+        <div className="themeSwitchContainer">
+          <img src={themeSwitchIcon} alt="" />
+        </div>
+      </div>
+
+      <div className="loginButton">GET IN</div>
     </div>
   );
 };
